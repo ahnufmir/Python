9005662567fd7843357316bba88d35a487386b50
ahnufmir <109764598+ahnufmir@users.noreply.github.com>
2025-04-11 21:26:13 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+courses_dictionary = {
+    'Alice' : {'Math', 'Science'},
+    'Bob' : {'Math', 'Science'},
+    'Chalrie' : {'Math'}
+}
+
+grades_dictionary = {
+    'Alice' : [85,92],
+    'Bob' : [90,88],
+    'Charlie' : [78]
+}
+
+sum_alice = 0
+
+for x in grades_dictionary['Alice']:
+    sum_alice = sum_alice + x
+
+avg_alice = sum_alice / len(grades_dictionary['Alice'])  
+
+sum_bob = 0
+
+for x in grades_dictionary['Bob']:
+    sum_bob  +=  x
+
+avg_bob = sum_bob  / len(grades_dictionary['Bob'])    
+
+sum_charlie = 0
+
+for x in grades_dictionary['Charlie']:
+    sum_charlie += x
+
+avg_charlie = sum_charlie / len(grades_dictionary['Charlie'])    
+
+
+avg_grade_dictionary = {
+    'Alice' : avg_alice,
+    'Bob' : avg_bob,
+    'Chalrie' : avg_charlie
+}
+
+print(avg_alice, avg_charlie, avg_bob)
+
+
